e7b5aacd37acf6361bbf3a254bdbf25ddf0a7dd0
dougie-hawes <dougiehawes@hotmail.com>
2020-11-28 12:59:58 +0000

    moved App into separate file. Fixed CSS

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import { Route } from "react-router-dom";
+
+import "style.min.css";
+
+import AdminDashboard from "components/routes/private/admin/AdminDashboard";
+import UserDashboard from "components/routes/private/user/UserDashboard";
+
+import Header from "components/layout/Header";
+
+import Login from "components/routes/auth/Login";
+import Signup from "components/routes/auth/Signup";
+
+import Landing from "components/routes/public/Landing";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      <div className="route-box">
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/admin" component={AdminDashboard} />
+        <Route exact path="/user" component={UserDashboard} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,11 @@
 import React from "react";
 
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { render } from "react-dom";
 
 import "style.css";
 
-import AdminDashboard from 'components/routes/private/admin/AdminDashboard'
-import UserDashboard from 'components/routes/private/user/UserDashboard'
-
-import Header from "components/layout/Header";
-
-import Login from "components/routes/auth/Login";
-import Signup from "components/routes/auth/Signup";
-
-import Landing from "components/routes/public/Landing";
-
-const App = () => {
-  return (
-    <div className="app">
-      <Header />
-      <div className="route-box">
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={Signup} />
-        <Route exact path='/admin' component={AdminDashboard} />
-        <Route exact path='/user' component={UserDashboard} />
-      </div>
-    </div>
-  );
-};
+import App from "./App";
 
 render(
   <BrowserRouter>
